ab49bd0207718936a5c3b2ebe6a3f94170b518f6
Justin Silverman <jsilve24@gmail.com>
2022-04-06 12:25:45 -0400

    random small changes

--- home/bin/get_exchange_cal.sh
+++ home/bin/get_exchange_cal.sh
@@ -5,7 +5,7 @@ WGET=/usr/bin/wget
 # ICS2ORG=~/.local/bin/ical2orgpy
 ICS2ORG=~/bin/ical2org.awk
 ICSFILE=~/.cache/ical2org/psu_cal.ics
-ORGFILE=~/Dropbox/org/exclude-beorg/cal-psu.org
+ORGFILE=~/Dropbox/org/cal-psu.org
 URL=https://outlook.office365.com/owa/calendar/83f9d82ee6474d86a4cf8d54b63a4d8f@psu.edu/14f437ce04884f40b18347008af154563232586173734917328/calendar.ics 
 
 
